70339920e8c813cd35799fe992d2ae4befd453dd
Fengjie Chen <cfj@Fengjies-MacBook-Pro.local>
2019-10-23 00:54:57 -0700

    modify problem 2

--- hw2.py
+++ hw2.py
@@ -10,15 +10,18 @@ def test_create_dataframe(df, colnames):
     :param colnames:  list[str]
     :return:          bool
     '''
-
     # test whether all columns are specified in colnames
     colnames_set = set(colnames)
     for col in df.columns:
         if col not in colnames_set:
             return False
-    # test whether all columns have same column types
-    if len(set(df.dtypes)) > 1:
-        return False
+    # test whether all elements in the same column have the same type
+    for col in df.columns:
+        for i in range(len(df[col])):
+            elem = df[col][i]
+            if type(elem) != type(df[col][0]):
+                return False
+
     # test whether there is at least 10 rows
     if df.shape[0] < 10:
         return False
@@ -30,6 +33,9 @@ if __name__ == '__main__':
     url = "https://data.seattle.gov/api/views/tw7j-dfaw/rows.csv?accessType=DOWNLOAD"
     df = reads(url)
     res = test_create_dataframe(df, list(df.columns))
-    assert res == False
-
     print(res)
+
+    d = {'col1': [1, 2], 'col2': [3, '3']}
+    df = pd.DataFrame(data=d)
+    print(test_create_dataframe(df, list(df.columns)))
+
